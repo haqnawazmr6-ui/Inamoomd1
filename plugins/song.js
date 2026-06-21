a8b375df5649402b74d2a04593e8f75d018617d1
NAWAZMD <haqnawazmr6@gmail.com>
2026-06-21 07:39:29 +0500

    Add YouTube music downloader command

---
+++ plugins/song.js
@@ -0,0 +1,72 @@
+const { cmd } = require("../command");
+const axios = require("axios");
+
+cmd({
+    pattern: "play",
+    alias: ["song", "music", "s", "audio"],
+    desc: "Smart YouTube music downloader",
+    category: "music",
+    react: "🔞",
+    filename: __filename
+},
+async (conn, mek, m, { from, args, reply }) => {
+
+    try {
+
+        if (!args[0]) {
+            return reply("❌ Song likho\nExample: .play faded");
+        }
+
+        let query = encodeURIComponent(args.join(" "));
+        let api = `https://api-xemoz-official.my.id/api/donwloader/ytplay.php?q=${query}`;
+
+        let { data } = await axios.get(api, { timeout: 15000 });
+
+        if (!data?.status) {
+            return reply("❌ Song not found");
+        }
+
+        const result = data.result;
+
+        if (!result?.download?.audio) {
+            return reply("❌ Audio not found");
+        }
+
+        let title = result.title;
+        let channel = result.channel;
+        let duration = result.duration;
+        let thumb = result.thumbnail;
+        let audioUrl = result.download.audio;
+
+        let text = `
+╭───────────────⭓
+│ 🎧 INAMOO MD
+╰───────────────⭓
+
+➤ 🎶 ${title}
+➤ 👤 ${channel}
+➤ ⏱ ${duration}
+
+╭───────────────⭓
+│ ⚡ Processing...
+│ 🚀 Sending Audio...
+╰───────────────⭓
+`.trim();
+
+        await conn.sendMessage(from, {
+            image: { url: thumb },
+            caption: text
+        }, { quoted: mek });
+
+        await conn.sendMessage(from, {
+            audio: { url: audioUrl },
+            mimetype: "audio/mp4",
+            ptt: false
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply("❌ Error in play command");
+    }
+
+});
